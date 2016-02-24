b18cc47e7b9544442ad13d3b49920300dc38291f
Anonymous <cryptofuture@users.noreply.github.com>
2016-02-24 07:40:05 +0000

    Update to idea style project

--- src/com/passcard/MainActivity.java
+++ src/com/passcard/MainActivity.java
@@ -1,73 +1,28 @@
 package com.passcard;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebView;
 
-@SuppressLint("SetJavaScriptEnabled")
-public class MainActivity extends ActionBarActivity {
-	
-WebView myWebView;
+public class MainActivity extends Activity {
+    private WebView WebViewPassCard;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		myWebView = (WebView) findViewById(R.id.webView1);
-		myWebView.getSettings().setBuiltInZoomControls(true);
-		myWebView.getSettings().setJavaScriptEnabled(true);
-		myWebView.getSettings().setDomStorageEnabled(true);
-		myWebView.loadUrl("file:///android_asset/passwordcard.html");
-		
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            WebViewPassCard.enableSlowWholeDocumentDraw();
+        }
+        setContentView(R.layout.mainland);
+        WebViewPassCard = (WebView) findViewById(R.id.webview);
+        WebViewPassCard.getSettings().setBuiltInZoomControls(true);
+        WebViewPassCard.getSettings().setJavaScriptEnabled(true);
+        WebViewPassCard.getSettings().setDomStorageEnabled(true);
+        WebViewPassCard.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        WebViewPassCard.loadUrl("file:///android_asset/passwordcard.html");
+    }
 }
